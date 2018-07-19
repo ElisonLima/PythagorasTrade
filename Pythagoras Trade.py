d5d860959c398ab619ee2bd341c424663dd1da7e
ElisonLima <limagoncalves@live.com>
2018-07-19 09:15:31 -0300

    Add files via upload

---
+++ Pythagoras Trade.py
@@ -0,0 +1,17 @@
+print('{:=^30}'.format('Pythagoras Trade'))
+
+coins = []
+price = []
+bid = float
+ask = float
+fee = float
+acoin = float
+bcoin = float
+ccoin = float
+d = (acoin/bcoin)*(bcoin/ccoin)*(ccoin/acoin)
+
+
+
+
+
+
